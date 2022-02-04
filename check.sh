ca9b411d4df1636e55368edb446e46fa961a61c1
oscar <oscar.picas@gmail.com>
2022-02-04 15:49:01 +0100

    Filter warnings

--- check.sh
+++ check.sh
@@ -1,6 +1,7 @@
 #!/bin/bash 
 
-output=$(go-licenses check "$(head -n 1 go.mod | awk '{ print $2 }')/..." 2>&1)
+# skip non-go code
+output=$(go-licenses check "$(head -n 1 go.mod | awk '{ print $2 }')/..." 2>&1 | grep -v W0204 | grep -v \.s)
 if [ -z "$output" ]; then
     echo "License Check Success"
     exit 0
--- csv.sh
+++ csv.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-go-licenses csv "$(head -n 1 go.mod | awk '{ print $2 }')/..." | tee licenses.csv
+go-licenses csv "$(head -n 1 go.mod | awk '{ print $2 }')/..." 2>/dev/null | tee licenses.csv
